de5c5c92bbd2662e488de7e8e73fe64bd895abbe
Vens <v.kolev@alfatrex.com>
2023-12-07 20:25:33 +0100

    LanguageContext fix

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect, Suspense, useCallback } from 'react';
+import React, { createContext, useState, useContext, useEffect, Suspense } from 'react';
 import { useRouter } from 'next/router';
 import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
@@ -13,47 +13,34 @@ export const LanguageProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
-  const changeLanguage = useCallback((newLanguage) => {
+  const changeLanguage = (newLanguage) => {
+    // Записване на предпочитанията за език в бисквитка
     Cookies.set(LANG_COOKIE_KEY, newLanguage, { expires: 365 });
+    // console.log('Cookie set:', Cookies.get(LANG_COOKIE_KEY));
     setLanguage(newLanguage);
 
+    // Актуализация на езика в i18n
     i18n.changeLanguage(newLanguage).then(() => {
-      setLoading(false);
+      setLoading(false); 
     });
-
+    // Променете пътя на текущия език
     router.push(`/${newLanguage}`);
-  }, [router]);
+  };
 
   useEffect(() => {
+    // Четене на предпочитания за език от бисквитка
     const preferredLanguage = Cookies.get(LANG_COOKIE_KEY) || 'en';
-    if (language !== preferredLanguage) {
-      changeLanguage(preferredLanguage);
-    }
-  }, [language, changeLanguage]);
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      // Извлечете новия език от URL и актуализирайте language state
-      const newLanguage = url.split('/')[1];
-      setLanguage(newLanguage);
-    };
-
-    // Слушайте промените на пътя
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    // Почистете слушателя при размонтиране на компонента
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router]);
+    changeLanguage(preferredLanguage);
+  }, []);  
 
   return (
-    loading ? <Loader /> :
+     loading ? <Loader /> :
     <LanguageContext.Provider value={{ language, changeLanguage }}>
       <Suspense fallback={<Loader />}>
         {children}
       </Suspense>
-    </LanguageContext.Provider>
+  </LanguageContext.Provider>
+
   );
 };
 

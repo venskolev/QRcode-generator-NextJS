3f1689220ad9514bd02daab8aa78b5c5386d6474
Vens <v.kolev@alfatrex.com>
2023-12-17 21:34:48 +0100

    Tabs add and fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Trans } from "react-i18next";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
+import Grid from "@mui/material/Grid";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
@@ -19,40 +20,71 @@ const Home = ({ lang }) => {
   return (
     <>
       <main id="content">
-        <div className="styleToggle">
-          <div>
-            <Trans i18nKey="welcome" components={[<b key={0} />]}></Trans>
-          </div>
-        </div>
-        <Box>
-          <TabContext value={value}>
-            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-              <TabList
-                onChange={handleChange}
-                aria-label="lab API tabs example"
-              >
-                <Tab label={<Trans i18nKey="url" />} value="1" />
-                <Tab label={<Trans i18nKey="text" />} value="2" />
-                <Tab label={<Trans i18nKey="vcard" />} value="3" />
-              </TabList>
-            </Box>
-            <TabPanel value="1">
-              <MainCard boxShadow title={<Trans i18nKey="QR Code Generator" />}>
-                <QrCodeGenerator />
-              </MainCard>
-            </TabPanel>
-            <TabPanel value="2">
-              <MainCard boxShadow title={<Trans i18nKey="QR Code Generator" />}>
-                <QrCodeGenerator />
-              </MainCard>
-            </TabPanel>
-            <TabPanel value="3">
-              <MainCard boxShadow title={<Trans i18nKey="QR Code Generator" />}>
-                <QrCodeGenerator />
-              </MainCard>
-            </TabPanel>
-          </TabContext>
-        </Box>
+        <TabContext value={value}>
+          <Grid
+            display={"flex"}
+            direction={"column"}
+            justifyContent={"center"}
+            flexWrap={"wrap"}
+            alignContent={"center"}
+            sx={{ minWidth: "100vw" }}
+          >
+            <MainCard
+              className="styleToggle"
+              title={<Trans i18nKey="welcome" components={[<b key={0} />]} />}
+            ></MainCard>
+          </Grid>
+          <Grid
+            container
+            xs={12} sm={6} md={12} lg={12} xl={12}
+            // display={"flex"}
+            // direction={"column"}
+            justifyContent={"center"}
+            // flexWrap={"wrap"}
+            alignContent={"center"}
+          >
+            <Grid item marginTop={"50px"}>
+              {/* <Box sx={{ borderBottom: 1, borderColor: "divider" }}> */}
+              <MainCard boxShadow title={<Trans i18nKey="menu" />}>
+                <TabList
+                  orientation="vertical"
+                  onChange={handleChange}
+                  aria-label="QR generators"
+                >
+                  <Tab label={<Trans i18nKey="url" />} value="1" />
+                  <Tab label={<Trans i18nKey="text" />} value="2" />
+                  <Tab label={<Trans i18nKey="vcard" />} value="3" />
+                </TabList></MainCard>
+              {/* </Box> */}
+            </Grid>
+            <Grid item>
+              <TabPanel value="1" sx={{ minWidth: "50vw" }}>
+                <MainCard
+                  boxShadow
+                  title={<Trans i18nKey="QR Code Generator" />}
+                >
+                  <QrCodeGenerator />
+                </MainCard>
+              </TabPanel>
+              <TabPanel value="2" sx={{ minWidth: "50vw" }}>
+                <MainCard
+                  boxShadow
+                  title={<Trans i18nKey="QR Code Generator" />}
+                >
+                  <QrCodeGenerator />
+                </MainCard>
+              </TabPanel>
+              <TabPanel value="3" sx={{ minWidth: "50vw" }}>
+                <MainCard
+                  boxShadow
+                  title={<Trans i18nKey="QR Code Generator" />}
+                >
+                  <QrCodeGenerator />
+                </MainCard>
+              </TabPanel>
+            </Grid>
+          </Grid>
+        </TabContext>
       </main>
     </>
   );
